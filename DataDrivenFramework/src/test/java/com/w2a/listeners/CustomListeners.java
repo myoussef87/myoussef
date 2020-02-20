b802d5b138a7ddca2c5d0027d5ad74d32d0b9a1b
myoussef87 <Utest1234>
2020-02-20 12:19:55 +0200

    Send Ectent reports by email

--- DataDrivenFramework/src/test/java/com/w2a/listeners/CustomListeners.java
+++ DataDrivenFramework/src/test/java/com/w2a/listeners/CustomListeners.java
@@ -1,7 +1,14 @@
 package com.w2a.listeners;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -10,11 +17,16 @@ import org.testng.SkipException;
 
 import com.relevantcodes.extentreports.LogStatus;
 import com.w2a.base.TestBase;
+import com.w2a.utilities.MonitoringMail;
+import com.w2a.utilities.TestConfig;
 import com.w2a.utilities.TestUtil;
 
-public class CustomListeners extends TestBase implements ITestListener {
+
+public class CustomListeners extends TestBase implements ITestListener,ISuiteListener {
+	public String messageBody;
 public void onFinish(ITestContext context) {
-	// TODO Auto-generated method stub
+
+
 	
 }
 public void onStart(ITestContext context) {
@@ -71,4 +83,30 @@ public void onTestSuccess(ITestResult result) {
 	   rep.flush();
 	
 	}
+public void onFinish(ISuite arg0) {
+	
+	MonitoringMail mail = new MonitoringMail();
+	 
+	try {
+		messageBody = "http://" + InetAddress.getLocalHost().getHostAddress()
+				+ ":8080/job/DataDrivenLiveProject/Extent_Reports/";
+	} catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	try {
+		mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+	} catch (AddressException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (MessagingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	
+}
+
+
 }
