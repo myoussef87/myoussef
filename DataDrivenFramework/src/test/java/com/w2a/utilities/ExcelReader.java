b44cbb87d38a8a327b323e8cc396212f909f8bd0
myoussef87 <Utest1234>
2020-02-24 17:06:33 +0200

    Update Excel Reader to be compatible to Apache latest versions

--- DataDrivenFramework/src/test/java/com/w2a/utilities/ExcelReader.java
+++ DataDrivenFramework/src/test/java/com/w2a/utilities/ExcelReader.java
@@ -3,14 +3,14 @@ package com.w2a.utilities;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Calendar;
 
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.common.usermodel.HyperlinkType;
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
@@ -93,9 +93,9 @@ public class ExcelReader {
 		if(cell==null)
 			return "";
 		
-		if(cell.getCellType()==Cell.CELL_TYPE_STRING)
+		if(cell.getCellType()==CellType.STRING)
 			  return cell.getStringCellValue();
-		else if(cell.getCellType()==Cell.CELL_TYPE_NUMERIC || cell.getCellType()==Cell.CELL_TYPE_FORMULA ){
+		else if(cell.getCellType()==CellType.NUMERIC || cell.getCellType()==CellType.FORMULA ){
 			  
 			  String cellText  = String.valueOf(cell.getNumericCellValue());
 			  if (HSSFDateUtil.isCellDateFormatted(cell)) {
@@ -117,7 +117,7 @@ public class ExcelReader {
 			  
 			  
 			  return cellText;
-		  }else if(cell.getCellType()==Cell.CELL_TYPE_BLANK)
+		  }else if(cell.getCellType()==CellType.BLANK)
 		      return ""; 
 		  else 
 			  return String.valueOf(cell.getBooleanCellValue());
@@ -152,9 +152,9 @@ public class ExcelReader {
 		if(cell==null)
 			return "";
 		
-	  if(cell.getCellType()==Cell.CELL_TYPE_STRING)
+	  if(cell.getCellType()==CellType.STRING)
 		  return cell.getStringCellValue();
-	  else if(cell.getCellType()==Cell.CELL_TYPE_NUMERIC || cell.getCellType()==Cell.CELL_TYPE_FORMULA ){
+	  else if(cell.getCellType()==CellType.NUMERIC || cell.getCellType()==CellType.FORMULA ){
 		  
 		  String cellText  = String.valueOf(cell.getNumericCellValue());
 		  if (HSSFDateUtil.isCellDateFormatted(cell)) {
@@ -176,7 +176,7 @@ public class ExcelReader {
 		  
 		  
 		  return cellText;
-	  }else if(cell.getCellType()==Cell.CELL_TYPE_BLANK)
+	  }else if(cell.getCellType()==CellType.BLANK)
 	      return "";
 	  else 
 		  return String.valueOf(cell.getBooleanCellValue());
@@ -294,7 +294,7 @@ public class ExcelReader {
 	    hlink_style.setFont(hlink_font);
 	    //hlink_style.setWrapText(true);
 
-	    XSSFHyperlink link = createHelper.createHyperlink(XSSFHyperlink.LINK_FILE);
+	    XSSFHyperlink link = createHelper.createHyperlink(HyperlinkType.FILE);
 	    link.setAddress(url);
 	    cell.setHyperlink(link);
 	    cell.setCellStyle(hlink_style);
@@ -360,8 +360,8 @@ public class ExcelReader {
 				return false;
 			
 		XSSFCellStyle style = workbook.createCellStyle();
-		style.setFillForegroundColor(HSSFColor.GREY_40_PERCENT.index);
-		style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+		style.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREY_40_PERCENT.getIndex());
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 		
 		sheet=workbook.getSheetAt(index);
 		
@@ -403,9 +403,9 @@ public class ExcelReader {
 		workbook = new XSSFWorkbook(fis);
 		sheet=workbook.getSheet(sheetName);
 		XSSFCellStyle style = workbook.createCellStyle();
-		style.setFillForegroundColor(HSSFColor.GREY_40_PERCENT.index);
-		XSSFCreationHelper createHelper = workbook.getCreationHelper();
-		style.setFillPattern(HSSFCellStyle.NO_FILL);
+		style.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREY_40_PERCENT.getIndex());
+		//XSSFCreationHelper createHelper = workbook.getCreationHelper();
+		style.setFillPattern(FillPatternType.NO_FILL);
 		
 	    
 	
@@ -499,18 +499,7 @@ public class ExcelReader {
 	}
 		
 	
-	// to run this on stand alone
-	public static void main(String arg[]) throws IOException{
-		
-		
-		ExcelReader datatable = null;
-		
-
-			 datatable = new ExcelReader("C:\\CM3.0\\app\\test\\Framework\\AutomationBvt\\src\\config\\xlfiles\\Controller.xlsx");
-				for(int col=0 ;col< datatable.getColumnCount("TC5"); col++){
-					System.out.println(datatable.getCellData("TC5", col, 1));
-				}
-	}
+	
 	
 	
 }
